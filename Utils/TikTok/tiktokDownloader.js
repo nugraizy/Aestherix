b25905cc6b01d07f403fc8e4de92191f0a78f9ab
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2022-07-08 07:29:50 +0000

    If Video Can't Di Download Maka Resolve Error

--- Utils/TikTok/tiktokDownloader.js
+++ Utils/TikTok/tiktokDownloader.js
@@ -2,18 +2,22 @@ import fetch from "node-fetch";
 
 export const tiktokDownloader = (url) =>
 	new Promise(async (resolve) => {
-		const data = await (await fetch(URL_KEY_PARSER(url))).json();
-		if (data.status !== "success") resolve(data);
-		const dataResult = await (await fetch(URL_DETAIL_PARSER(data.data.key))).json();
-		if (dataResult.status !== "success") resolve(dataResult);
-		resolve({
-			...dataResult.data.author,
-			description: dataResult.data.description,
-			with_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.with_watermark),
-			no_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.no_watermark),
-			no_watermark_raw: dataResult.data.video.no_watermark_raw,
-			music: URL_BASE_MUSIC(dataResult.data.music),
-		});
+		try {
+			const data = await (await fetch(URL_KEY_PARSER(url))).json();
+			if (data.status !== "success") resolve(data);
+			const dataResult = await (await fetch(URL_DETAIL_PARSER(data.data.key))).json();
+			if (dataResult.status !== "success") resolve(dataResult);
+			resolve({
+				...dataResult.data.author,
+				description: dataResult.data.description,
+				with_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.with_watermark),
+				no_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.no_watermark),
+				no_watermark_raw: dataResult.data.video.no_watermark_raw,
+				music: URL_BASE_MUSIC(dataResult.data.music),
+			});
+		} catch (e) {
+			resolve(e)
+		}
 	});
 const URL_KEY_PARSER = (input) => `https://api.ngutek.com/video-key?video_url=${input}`;
 const URL_DETAIL_PARSER = (input) => `https://api.ngutek.com/video-details-by-key?key=${input}`;
