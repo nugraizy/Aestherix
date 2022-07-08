1374628e865175b5c4482cb88cd86277b5054cd6
nugraizy <devijimmy1977@gmail.com>
2022-07-08 15:56:47 +0700

    CONFLICT.

--- Utils/TikTok/tiktokDownloader.js
+++ Utils/TikTok/tiktokDownloader.js
@@ -1,23 +1,7 @@
 import fetch from "node-fetch";
-const URL = "https://vm.tiktok.com/ZSRJjDsgB/?k=1";
 
 export const tiktokDownloader = (url) =>
 	new Promise(async (resolve) => {
-<<<<<<< HEAD
-		url = url.includes("vm.tiktok.com") ? url.replace("vm.tiktok.com", "vt.tiktok.com") : url;
-		const data = await (await fetch(URL_KEY_PARSER(url))).json();
-		if (data.status !== "success") resolve(data);
-		const dataResult = await (await fetch(URL_DETAIL_PARSER(data.data.key))).json();
-		if (dataResult.status !== "success") resolve(dataResult);
-		resolve({
-			...dataResult.data.author,
-			description: dataResult.data.description,
-			with_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.with_watermark),
-			no_watermark: URL_BASE_DOWNLOAD(dataResult.data.video.no_watermark),
-			no_watermark_raw: dataResult.data.video.no_watermark_raw,
-			music: URL_BASE_MUSIC(dataResult.data.music),
-		});
-=======
 		try {
 			url = url.includes("vm.tiktok.com") ? url.replace("vm.tiktok.com", "vt.tiktok.com") : url;
 			const data = await (await fetch(URL_KEY_PARSER(url))).json();
@@ -33,9 +17,8 @@ export const tiktokDownloader = (url) =>
 				music: URL_BASE_MUSIC(dataResult.data.music),
 			});
 		} catch (e) {
-			resolve(e)
+			resolve(e);
 		}
->>>>>>> b25905cc6b01d07f403fc8e4de92191f0a78f9ab
 	});
 const URL_KEY_PARSER = (input) => `https://api.ngutek.com/video-key?video_url=${input}`;
 const URL_DETAIL_PARSER = (input) => `https://api.ngutek.com/video-details-by-key?key=${input}`;
