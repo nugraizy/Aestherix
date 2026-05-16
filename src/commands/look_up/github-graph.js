569d7f096caa97fc318265e1132a7fac1e92ee35
nugraizy <devijimmy1977@gmail.com>
2026-05-16 23:13:13 +0700

    fix(commands): correct github graph theme retry payload

--- src/commands/look_up/github-graph.js
+++ src/commands/look_up/github-graph.js
@@ -1,7 +1,8 @@
 import parser from 'yargs-parser';
 import { GitHubGraph } from '../../helper/index.js';
+import { cmdId } from '../../helper/modules/prefix.js';
 
-let themes = null;
+let themes = [];
 
 /**
  * @type {import('../../types/Commands/index.js').CommandProps}
@@ -16,7 +17,7 @@ export default {
 	cooldown: 6,
 	limit: 3,
 	status: 'enable',
-	async run({ from, query, message }, client) {
+	async run({ from, query, message, cmd }, client) {
 		if (!query) {
 			return await client.reply(from, 'Please specify a GitHub User', message);
 		}
@@ -31,8 +32,9 @@ export default {
 		});
 
 		const git = new GitHubGraph();
+		const usernameQuery = username.join(' ').trim();
 
-		if (!themes) {
+		if (!themes.length) {
 			themes = Object.keys(git.themes).map((v) => v.toLowerCase());
 		}
 
@@ -44,20 +46,27 @@ export default {
 			return await client.reply(from, `List of themes:\n\n${themes.join(', ')}`, message);
 		}
 
-		if (!themes.includes(theme.toLowerCase())) {
-			return await client.reply(
-				from,
-				`Please specify a valid themes, this is a list of available themes:\n\n${themes.join(', ')}`,
-				message
-			);
+		if (!themes.includes(theme?.toLowerCase())) {
+			const builder = new client.TemplateBuilder.Native();
+
+			return await builder
+				.destination(from)
+				.footer(`Use ${cmd} ${query} --theme ${themes.random()} to create graph with specific theme.`)
+				.body(`Please specify a valid themes, this is a list of available themes:\n\n${themes.join(', ')}`)
+				.buttons(builder.button.reply({ display: 'Create Default Graph', id: cmdId(cmd, `${usernameQuery} --theme default`) }))
+				.send();
 		}
 
-		const init = await git.init(username, { round: true, theme: theme.toUpperCase(), backgroundMesh: true });
+		const wait = await client.waitMessage(from, 'Creating graph. Please wait...', message);
+
+		const init = await git.init(username, { round: true, theme: theme.toUpperCase() });
 
 		const create = await init.createGitHubGraph();
 
 		const buffer = create.toBuffer();
 
 		await client.send(from, { image: new Buffer.from(buffer) }, { quoted: message });
+
+		await wait.update('Graph created successfully!');
 	}
 };
